86594bc56f700fc8ca9ff144d497df4daaa4473a
Limitless HQ <74993912+limitless-hq@users.noreply.github.com>
2022-02-04 22:52:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
