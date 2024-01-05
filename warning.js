bef3a56b247816dce6db24e38afa413a4b156c52
Limitless HQ <74993912+limitless-hq@users.noreply.github.com>
2024-01-05 17:14:55 -0500

    Update warning.js
    changed message

--- warning.js
+++ warning.js
@@ -1,5 +1,5 @@
 const buttonPressed = document.querySelector('.surprise');
 
 addEventListener('click', function (){
-    buttonPressed.classList.toggle(this.alert('WARNING! This site is Pushing P'))
+    buttonPressed.classList.toggle(this.alert('WARNING!'))
 })
